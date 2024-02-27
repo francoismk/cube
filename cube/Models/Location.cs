8c134cc600becdf7e7c6edced4bc64af37de14f7
francoismk <74190910+francoismk@users.noreply.github.com>
2024-02-27 20:29:41 +0100

    feat: connecting db and run first migration

--- cube/Models/Location.cs
+++ cube/Models/Location.cs
@@ -5,4 +5,6 @@ public class Location
     public int Id { get; set; }
     public required string LocationName { get; set; }
     public int ZipCode { get; set; }
+
+    public required ICollection<Service> Services { get; set; }
 }
