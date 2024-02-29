bb52f689c6494c2332f8dc3e65dedbec3a4e2ef5
francoismk <74190910+francoismk@users.noreply.github.com>
2024-02-29 09:07:58 +0100

    feat: wip on service controller et repository

--- cube/Controllers/ServiceController.cs
+++ cube/Controllers/ServiceController.cs
@@ -10,4 +10,9 @@ public class ServiceController : ControllerBase
   private readonly IRepositoryData<Service> ?_serviceRepository;
 
   private readonly IRepositoryData<Employee> ?_employeeRepository;
+
+  public ServiceController(IRepositoryData<Service> serviceRepository, IRepositoryData<Employee> employeeRepository) {
+    _serviceRepository = serviceRepository;
+    _employeeRepository = employeeRepository;
+  }
 }
